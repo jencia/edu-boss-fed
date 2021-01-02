bf6e69ec784c25a0f84d75aef83192c24233d57c
jencia <1073956837@qq.com>
2021-01-02 21:26:58 +0800

    feat: 封装列表页面用于表单筛选的组件

---
+++ src/components/MeListForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <el-card class="condition-block">
+    <el-form inline @submit.native.prevent>
+      <el-form-item v-for="item in data" :key="item.field" :label="item.label">
+        <el-input size="small" v-model="formData[item.field]" v-if="!item.type" clearable />
+        <el-select v-if="item.type === 'select'" v-model="formData[item.field]" clearable>
+          <el-option
+            v-for="optItem in item.options"
+            :key="optItem.value"
+            :label="optItem.label"
+            :value="optItem.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button size="small" type="primary" @click="handleSearch">查询</el-button>
+        <el-button size="small" @click="handleReset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  name: 'MeListForm',
+  props: {
+    data: {
+      type: Array as PropType<{
+        label: string,
+        field: string,
+        type?: '' | 'select',
+        options?: { label: string, value: string }[]
+      }[]>,
+      required: true
+    },
+    onSearch: {
+      type: Function,
+      required: true
+    }
+  },
+  data () {
+    return {
+      formData: this.data.reduce((rs, item) => {
+        rs[item.field] = ''
+        return rs
+      }, {} as Record<string, string>)
+    }
+  },
+  methods: {
+    handleReset () {
+      this.formData = this.data.reduce((rs, item) => {
+        rs[item.field] = ''
+        return rs
+      }, {} as Record<string, string>)
+      this.onSearch(this.formData)
+    },
+    handleSearch () {
+      this.onSearch(this.formData)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.condition-block {
+  margin-bottom: 10px;
+  &::v-deep .el-card__body {
+    padding-bottom: 0;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
+import MeListForm from '@/components/MeListForm.vue'
+import dayjs from 'dayjs'
 import './styles/index.scss'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.component('MeListForm', MeListForm)
+Vue.filter('date', (value: string) => {
+  return dayjs(value).format('YYYY-MM-DD HH:mm:ss')
+})
 
 new Vue({
   router,
